d810d18dbf3d4337ab050def1bac571c009d3541
Xee-sudo <68473853+Xee-sudo@users.noreply.github.com>
2020-07-19 15:10:05 +0700

    XMAN
    1 of all~

---
+++ README.py
@@ -0,0 +1,8 @@
+import os, sys
+
+os.system("clear")
+os.system("figlet XMAN")
+print("สวัสดีขอบคุณที่คุณอ่าน")
+print("ตัวสคริปต์นี้สร้างมาเพื่อReinstall")
+print("จัดทำโดยตัวคนเดียว")
+print("by MaG7")
---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install figlet
+
+echo"ติดตั้งอุปกรณ์ เสริม 1อย่างแล้ว!"
+
+pkg install git 
+
+echo"ตั้งตั้งอุตั้งตั้งอุปกรณ์ เสริม 2อย่างแล้ว!"
+
+echo"ขั้นตอนต่อต่อไปให้ sh Xman เพื่อ Super Install ทุกโปรแกรม"
