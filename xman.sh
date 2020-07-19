b57027e8b826fd9b2d1b644cfe05dd0f96b98b01
Xee-sudo <68473853+Xee-sudo@users.noreply.github.com>
2020-07-19 19:04:26 +0700

    Add files via upload

---
+++ xman.sh
@@ -0,0 +1,45 @@
+figlet X M A N !
+
+
+echo"== SUPER INSTALL =="
+
+git clone https://github.com/tatajub11465/SMS/
+echo"SMS INSTALL"
+git clone https://github.com/Ha3Mrx/DDos-Attack
+echo"DDOS INSTALL"
+git clone https://github.com/rajkumardusad/IP-Tracer
+echo"IP-Tracer"
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+echo"Cam-Hackers"
+git clone https://github.com/Bhai4You/Termux-Banner
+echo"Termux-Banner")
+
+echo"Reinstall"
+pkg install python
+
+pkg install python2
+
+pkg install python3
+
+pkg install php
+
+apt update
+
+apt upgrade
+
+apt install git
+
+echo"install List
+pkg install python
+
+pkg install python2
+
+pkg install python3
+
+pkg install php
+
+apt update
+
+apt upgrade
+
+apt install git"
